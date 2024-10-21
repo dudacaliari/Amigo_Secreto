2b380d80949f4d67755ddbdcfc341693c54d64da
Eduarda Caliari <eduarda.dcaliari@gmail.com>
2024-10-21 13:27:58 -0300

    Criação do Projeto e da Tabela Pessoa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PessoaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PessoaController::class, 'index'])->name('home');
+Route::get('/pessoa/create', [PessoaController::class, 'create'])->name('pessoa.create');
+Route::post('/pessoa', [PessoaController::class, 'store'])->name('pessoa.store');
+Route::get('/pessoa/{id}/edit', [PessoaController::class, 'edit'])->name('pessoa.edit');
+Route::put('/pessoa/{id}', [PessoaController::class, 'update'])->name('pessoa.update');
+Route::delete('/pessoa/{id}', [PessoaController::class, 'destroy'])->name('pessoa.destroy');
+Route::get('/sorteio', [PessoaController::class, 'sorteio'])->name('sorteio');
+
