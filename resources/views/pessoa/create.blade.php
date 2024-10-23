5758eb382c81dca7ef7b2fe16780d38fd1a32fb3
Eduarda Caliari <eduarda.dcaliari@gmail.com>
2024-10-22 21:46:40 -0300

    Separando Arquivos JS

--- resources/views/pessoa/create.blade.php
+++ resources/views/pessoa/create.blade.php
@@ -61,27 +61,7 @@
         <button type="submit" class="btn btn-primary" id="saveButton">Salvar</button>
     </form>
 
-    <script>
-        function validateForm() {
-            const form = document.querySelector('form');
-            const button = document.getElementById('saveButton');
-            button.disabled = !form.checkValidity();
-            const nome = document.getElementById('nome').value;
-            const sobrenome = document.getElementById('sobrenome').value;
-            const email = document.getElementById('email').value;
-            const saveButton = document.getElementById('saveButton');
-
-            // Habilitar botão somente se todos os campos obrigatórios estiverem preenchidos
-            if (nome.length >= 3 && sobrenome.length >= 3 && email) {
-                saveButton.disabled = false;
-            } else {
-                saveButton.disabled = true;
-            }
-        }
-
-        // Inicializa o estado do botão salvar
-        document.addEventListener("DOMContentLoaded", function() {
-            validateForm();
-        });
-    </script>
+    <!-- Incluindo os arquivos CSS e JS -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="{{ asset('js/scripts.js') }}"></script>
 @endsection
--- resources/views/pessoa/edit.blade.php
+++ resources/views/pessoa/edit.blade.php
@@ -63,22 +63,7 @@
         <button type="submit" class="btn btn-primary" id="saveButton">Salvar</button>
     </form>
 
-    <script>
-        function validateForm() {
-            const form = document.querySelector('form');
-            const button = document.getElementById('saveButton');
-            button.disabled = !form.checkValidity();
-            const nome = document.getElementById('nome').value;
-            const sobrenome = document.getElementById('sobrenome').value;
-            const email = document.getElementById('email').value;
-
-            // Habilitar botão somente se todos os campos obrigatórios estiverem preenchidos
-            button.disabled = !(nome.length >= 3 && sobrenome.length >= 3 && email);
-        }
-
-        // Inicializa o estado do botão salvar
-        document.addEventListener("DOMContentLoaded", function() {
-            validateForm();
-        });
-    </script>
+    <!-- Incluindo os arquivos CSS e JS -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="{{ asset('js/scripts.js') }}"></script>
 @endsection
