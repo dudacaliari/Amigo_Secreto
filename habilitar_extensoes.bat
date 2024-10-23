3bd48e9e06106bd2279098792fed4630c65b2c23
Eduarda Caliari <eduarda.dcaliari@gmail.com>
2024-10-23 12:12:18 -0300

    Arrumando arquivo para habilitar extensões

--- habilitar_extensoes.bat
+++ habilitar_extensoes.bat
@@ -8,6 +8,11 @@ powershell -Command "(gc %phpIni%) -replace ';extension=fileinfo', 'extension=fi
 powershell -Command "(gc %phpIni%) -replace ';extension=mbstring', 'extension=mbstring' | Out-File %phpIni% -Encoding UTF8"
 powershell -Command "(gc %phpIni%) -replace ';extension=openssl', 'extension=openssl' | Out-File %phpIni% -Encoding UTF8"
 powershell -Command "(gc %phpIni%) -replace ';extension=pdo_mysql', 'extension=pdo_mysql' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=zip', 'extension=zip' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=curl', 'extension=curl' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=xml', 'extension=xml' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=soap', 'extension=soap' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=gd', 'extension=gd' | Out-File %phpIni% -Encoding UTF8"
 
 echo Extensões habilitadas com sucesso no php.ini!
 echo Reinicie o Apache para aplicar as alterações.
