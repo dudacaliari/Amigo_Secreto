d7e241d14afe4b688b2fb93f89f5ef48352f01aa
Eduarda Caliari <eduarda.dcaliari@gmail.com>
2024-10-23 12:04:34 -0300

    Adicionando arquivo para habilitar extensões

---
+++ habilitar_extensoes.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Defina o caminho para o php.ini
+set "phpIni=C:\xampp\php\php.ini"
+
+REM Habilitar extensões necessárias no php.ini
+powershell -Command "(gc %phpIni%) -replace ';extension=intl', 'extension=intl' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=fileinfo', 'extension=fileinfo' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=mbstring', 'extension=mbstring' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=openssl', 'extension=openssl' | Out-File %phpIni% -Encoding UTF8"
+powershell -Command "(gc %phpIni%) -replace ';extension=pdo_mysql', 'extension=pdo_mysql' | Out-File %phpIni% -Encoding UTF8"
+
+echo Extensões habilitadas com sucesso no php.ini!
+echo Reinicie o Apache para aplicar as alterações.
+pause
